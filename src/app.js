1b699fc0e9ad92bdac70b25a3ffec6954e73a2ee
Miguel Martinez <mike280715@gmail.com>
2024-05-11 18:14:43 -0600

    nombre de la db

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 app.use(router);
 app.get('/', (req, res) => {
-    return res.send("Welcome to express!");
+    return res.send("Welcome to Movies App!");
 })
 
 // middlewares después de las rutas
